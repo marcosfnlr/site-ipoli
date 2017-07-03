deed720c1f758579e88823d1b1191ae9c48efd6d
Marcos Felipe xD <marcosfnlr@gmail.com>
2017-07-03 00:09:15 -0300

    Primeira tentativa de menu

--- WebContent/scripts/base.js
+++ WebContent/scripts/base.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
 	$(window).resize(function() {
 		$(".navbar").attr("data-offset-top", $(".header").outerHeight() - 3);
 	});
+	$('.dropdown-submenu a.test').on("click", function(e){
+	    $(this).next('ul').toggle();
+	    e.stopPropagation();
+	    e.preventDefault();
+	  });
 });
